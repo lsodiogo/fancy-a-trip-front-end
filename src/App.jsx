43442df22df6c76c16bbb5e4cfb2179acce76866
Diogo Lima <75363687+diogooslima@users.noreply.github.com>
2023-12-07 18:59:37 +0100

    header and footer added: html, css (work in progress)

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,33 @@ function App() {
 
   return (
     <>
-      <p>
-        HELLO WORLD
-      </p>
+      <header class="header">
+        <a href="#" class="logo">FANCY A TRIP</a>
+
+        <div class="header-right">
+          <a class="active" href="#">Home</a>
+          <a href="#">New Travel Location</a>
+          <a href="#">About</a>
+        </div>
+      </header>
+
+      <footer class="footer">
+        <div class="social">
+          <a href="#">
+            <img class="home" src="/public/images/home.svg" alt=""/>
+          </a>
+
+          <a href="#">
+            <img class="email" src="/public/images/email.svg" alt=""/>
+          </a>
+
+          <a href="#">
+            <img class="instagram" src="/public/images/instagram.svg" alt=""/>
+          </a>
+        </div>
+
+        <p class="copyright">Diogo Lima @ 2024</p>
+      </footer>
     </>
   )
 
